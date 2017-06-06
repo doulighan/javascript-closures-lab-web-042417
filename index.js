610ba0e3ac1d9ed5a445b0b5dc51512311b49dcc
doulighan <doulighan@gmail.com>
2017-06-06 14:38:21 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const app = "I don't do much."
+
+function bumpCounter () {
+  var counter = 0
+
+  function addBump () {
+    return counter += 1
+  }
+
+  function getBumps () {
+    return counter
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal (animalType) {
+  function attachDevice (deadlyDevice) {
+    return { animalType, deadlyDevice }
+  }
+  return attachDevice
+}
+
+function sharkCreator () {
+  return createAnimal('Shark')
+}
+
+var sharkWithFrickinLaserbeam = sharkCreator()('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator()('Cannon')
--- test/root.js
+++ test/root.js
@@ -1,23 +1,23 @@
-global.expect = require('expect');
+global.expect = require('expect')
 
-const babel = require('babel-core');
-const jsdom = require('jsdom');
-const path = require('path');
+const babel = require('babel-core')
+const jsdom = require('jsdom')
+const path = require('path')
 
-before(function(done) {
+before(function (done) {
   const babelResult = babel.transformFileSync(path.resolve(__dirname, '..', 'index.js'), {
     presets: ['es2015']
-  });
+  })
 
   jsdom.env(path.resolve(__dirname, '..', 'index.html'), [], {src: babelResult.code}, (err, window) => {
     if (err) {
-      return done(err);
+      return done(err)
     }
 
     Object.keys(window).forEach(key => {
-      global[key] = window[key];
-    });
+      global[key] = window[key]
+    })
 
-    return done();
-  });
-});
+    return done()
+  })
+})
